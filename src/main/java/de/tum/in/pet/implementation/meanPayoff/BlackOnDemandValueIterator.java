ac4b3efd5a505315a7c48a3807aba39040230169
Vipul Cariappa <vipulcariappa@gmail.com>
2025-06-15 20:17:41 +0200

    Implement `InPlaceBettingMartingale` alongside with `BettingMartingale`

--- src/main/java/de/tum/in/pet/implementation/meanPayoff/BlackOnDemandValueIterator.java
+++ src/main/java/de/tum/in/pet/implementation/meanPayoff/BlackOnDemandValueIterator.java
@@ -6,13 +6,13 @@ import de.tum.in.pet.implementation.reachability.BlackUnboundedReachValues;
 import de.tum.in.pet.sampler.UnboundedValues;
 import de.tum.in.pet.util.BettingMartingale;
 import de.tum.in.pet.util.ErrorProbabilityCalculator;
+import de.tum.in.pet.util.InPlaceBettingMartingale;
 import de.tum.in.pet.util.VectorDouble;
 import de.tum.in.pet.values.Bounds;
 import de.tum.in.probmodels.explorer.BlackExplorer;
 import de.tum.in.probmodels.explorer.Explorer;
 import de.tum.in.probmodels.generator.RewardGenerator;
 import de.tum.in.probmodels.graph.Mec;
-import de.tum.in.probmodels.model.Action;
 import de.tum.in.probmodels.model.Distribution;
 import de.tum.in.probmodels.model.Model;
 import it.unimi.dsi.fastutil.doubles.Double2LongFunction;
@@ -54,6 +54,8 @@ public class BlackOnDemandValueIterator<S, M extends Model> extends OnDemandValu
   protected static final double initialNSamples = 1e4;
   protected static final double multiplicativeFactor = 5;
 
+  protected HashMap<Integer, HashMap<Integer, HashMap<Integer, Pair<InPlaceBettingMartingale, Pair<Double, Double>>>>> in_place_martingale = new HashMap<>();
+
   public BlackOnDemandValueIterator(Explorer<S, M> explorer, UnboundedValues values, RewardGenerator<S> rewardGenerator,
                                     int revisitThreshold, double rMax, double pMin, double errorTolerance,
                                     Double2LongFunction nSampleFunction, double precision, long timeout,
@@ -86,8 +88,11 @@ public class BlackOnDemandValueIterator<S, M extends Model> extends OnDemandValu
         return 0d;
       }
       HashMap<Integer, HashMap<Integer, Pair<ArrayList<Integer>, Pair<Double, Double>>>> actionHash = martingaleTransitions.get(state);
+      HashMap<Integer, HashMap<Integer, Pair<InPlaceBettingMartingale, Pair<Double, Double>>>> actionMartingale = in_place_martingale.get(state);
       if (actionHash == null) { return 1d; }
+
       HashMap<Integer, Pair<ArrayList<Integer>, Pair<Double, Double>>> nextStateHash = actionHash.get(action);
+      HashMap<Integer, Pair<InPlaceBettingMartingale, Pair<Double, Double>>> nextStateMartingale = actionMartingale.get(action);
       if (nextStateHash == null) { return 1d; }
 
       double max = Double.NEGATIVE_INFINITY;
@@ -103,6 +108,18 @@ public class BlackOnDemandValueIterator<S, M extends Model> extends OnDemandValu
         pair.getValue().second.first = valuePair.first;
         pair.getValue().second.second = valuePair.second;
       }
+      double max_in_place_martingale = Double.NEGATIVE_INFINITY;
+      for (var pair: nextStateMartingale.entrySet()) {
+        InPlaceBettingMartingale martingale = pair.getValue().first;
+        de.tum.in.pet.util.Pair<Double, Double> valuePair = martingale.confidence_width(100, pair.getValue().second.first, pair.getValue().second.second, true, 0.5, 0.5);
+        double value = valuePair.second - valuePair.first;
+        if (max_in_place_martingale < value) {
+          max_in_place_martingale = value;
+        }
+        pair.getValue().second.first = valuePair.first;
+        pair.getValue().second.second = valuePair.second;
+      }
+      assert max_in_place_martingale == max;
       return max;
     });
 
@@ -120,19 +137,28 @@ public class BlackOnDemandValueIterator<S, M extends Model> extends OnDemandValu
     // TODO: `action` is `nextActionIndex` from the caller side, therefore we can
     //       optimise `martingaleTransitions` as (int -> array -> array) instead of (int -> int -> int -> array)
     martingaleTransitions.putIfAbsent(currentState, new HashMap<>());
+    in_place_martingale.putIfAbsent(currentState, new HashMap<>());
     HashMap<Integer, HashMap<Integer, Pair<ArrayList<Integer>, Pair<Double, Double>>>> actionHash = martingaleTransitions.get(currentState);
+    HashMap<Integer, HashMap<Integer, Pair<InPlaceBettingMartingale, Pair<Double, Double>>>> actionMartingales = in_place_martingale.get(currentState);
 
     actionHash.putIfAbsent(actionIndex, new HashMap<>());
+    actionMartingales.putIfAbsent(actionIndex, new HashMap<>());
     HashMap<Integer, Pair<ArrayList<Integer>, Pair<Double, Double>>> nextStateHash = actionHash.get(actionIndex);
+    HashMap<Integer, Pair<InPlaceBettingMartingale, Pair<Double, Double>>> nextStateMartingale = actionMartingales.get(actionIndex);
 
     // Get all possible nextStates
     explorer.getActions(currentState).get(actionIndex).distribution().forEach((s, d) -> {
       nextStateHash.putIfAbsent(s, new Pair<>(new ArrayList<>(), new Pair<>(0.0, 1.0)));
+      nextStateMartingale.putIfAbsent(s, new Pair<>(new InPlaceBettingMartingale(0.05, 0.5, 0.25, 1, 1), new Pair<>(0.0, 1.0)));
+
       ArrayList<Integer> distribution = nextStateHash.get(s).first;
+      InPlaceBettingMartingale martingale = nextStateMartingale.get(s).first;
       if (s == nextState) {
         distribution.add(1);
+        martingale.AddObservation(1);
       } else {
         distribution.add(0);
+        martingale.AddObservation(0);
       }
     });
   }
---
+++ src/main/java/de/tum/in/pet/util/InPlaceBettingMartingale.java
@@ -0,0 +1,59 @@
+package de.tum.in.pet.util;
+
+public class InPlaceBettingMartingale {
+    VectorDouble samples = new VectorDouble();
+    long samples_count;
+    double alpha;
+    double prior_mean;
+    double prior_variance;
+    double fake_obs;
+    double scale;
+
+    VectorDouble samples_cumulative_sum;
+    VectorDouble samples_mean_diff_sq;
+    VectorDouble mu_hat_t = new VectorDouble();
+    VectorDouble sigma2_t;
+    VectorDouble lambda = new VectorDouble();
+
+    public InPlaceBettingMartingale(double alpha, double prior_mean, double prior_variance, double fake_obs, double scale) {
+        this.alpha = alpha;
+        this.prior_mean = prior_mean;
+        this.prior_variance = prior_variance;
+        this.fake_obs = fake_obs;
+        this.scale = scale;
+        this.samples_count = 0;
+        this.samples_cumulative_sum = new VectorDouble(0);
+        this.samples_mean_diff_sq = new VectorDouble(0);
+        this.sigma2_t = new VectorDouble(prior_variance);
+    }
+
+    public void AddObservation(double sample) {
+        samples = VectorDouble.append(samples, sample);
+        samples_count += 1;
+
+        double sample_cumulative_sum = sample + samples_cumulative_sum.at(samples_cumulative_sum.size() - 1);
+        samples_cumulative_sum = VectorDouble.append(samples_cumulative_sum, sample_cumulative_sum);
+
+        // lazy compute mut_hat_t
+        double mu_hat_t_i = Math.min((sample_cumulative_sum + fake_obs + prior_mean) / (samples_count + fake_obs), 1);
+        mu_hat_t = VectorDouble.append(mu_hat_t, mu_hat_t_i);
+        
+        // lazy compute sigma2_t
+        double sample_mean_diff_sq = Math.pow(sample - mu_hat_t_i, 2) + samples_mean_diff_sq.at(samples_mean_diff_sq.size() - 1);
+        samples_mean_diff_sq = VectorDouble.append(samples_mean_diff_sq, sample_mean_diff_sq);
+        double sigma2_t_i = ((sample_mean_diff_sq) + (fake_obs * prior_variance)) / (samples_count + fake_obs);
+        sigma2_t = VectorDouble.append(sigma2_t, sigma2_t_i);
+
+        // lazy compute lambda
+        double lambda_i = Math.sqrt((2 * Math.log(1 / alpha)) / ((samples.size() * Math.log(samples.size() + 1)) * (sigma2_t_i)));
+        lambda = VectorDouble.append(lambda, lambda_i);
+    }
+
+    public Pair<Double, Double> confidence_width(int breaks, double break_start, double break_stop, boolean running_intersection, double theta, double trunc_scale) {
+        Pair<VectorDouble, VectorDouble> r = BettingMartingale.confidence_sequence_from_martingale(samples, lambda, breaks, break_start, break_stop, alpha, running_intersection, theta, trunc_scale);
+        VectorDouble l = r.first;
+        VectorDouble u = r.second;
+        // return VectorDouble.index(u, u.size() - 1) - VectorDouble.index(l, l.size() - 1);
+        return new Pair<>(VectorDouble.index(l, l.size() - 1), VectorDouble.index(u, u.size() - 1));
+    }
+}
